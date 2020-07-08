649552734e7afcac6b63bf647b5cfdab401eb6b0
truizdiaz <tomiruizdiaz@gmail.com>
2020-07-08 15:04:28 -0300

    Iteracion de empleados

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../models/empleado';
 
 @Component({
   selector: 'app-empleados',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empleados.component.css']
 })
 export class EmpleadosComponent implements OnInit {
+  listEmpleados: Empleado[] = [
+    { legajo: 1, nombre: 'Juan', apellido: 'Perez', genero: 'Masculino', salario: 70000 },
+    { legajo: 2, nombre: 'Maria', apellido: 'Aliaga', genero: 'Femenino', salario: 85000 },
+    { legajo: 3, nombre: 'Nicolas', apellido: 'Gomez', genero: 'Masculino', salario: 90000 },
+    { legajo: 4, nombre: 'Fernando', apellido: 'Cortes', genero: 'Masculino', salario: 55000 },
+    { legajo: 5, nombre: 'Rocio', apellido: 'Escudero', genero: 'Femenino', salario: 60000 },
+   ];
+
 
   constructor() { }
 
---
+++ src/app/models/empleado.ts
@@ -0,0 +1,7 @@
+export class Empleado {
+    legajo: number;
+    nombre: string;
+    apellido: string;
+    genero: string;
+    salario: number;
+}
